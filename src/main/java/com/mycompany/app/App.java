b9d7930c98fbba1977f7a7ddd1aa979191766f92
NgWengMun <weng-mun.ng@keysight.com>
2024-02-20 16:33:49 +0800

    version 3

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Hello World Version 2!!!");
+        System.out.println("Hello World Version 3!!!");
     }
 
     public String getMessage() {
