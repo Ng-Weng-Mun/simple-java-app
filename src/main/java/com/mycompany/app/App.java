0e8968dc56ff8cd1f5558040f24eb192dd449bc9
NgWengMun <weng-mun.ng@keysight.com>
2024-02-21 09:26:38 +0800

    Some changes to test webhooks

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Hello World Version 3!!!");
+        System.out.println("Hello World Version 4!!!");
     }
 
     public String getMessage() {
